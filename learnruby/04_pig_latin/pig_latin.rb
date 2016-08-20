e67a760fd5d9d51615001ae655e1e9bb1b126936
Nathan McKinley-Pace <nmckinl3@gmu.edu>
2016-08-20 14:57:56 -0400

    Initiated development on pig_latin.rb and set focus on searchbar in google.html

--- learnruby/04_pig_latin/pig_latin.rb
+++ learnruby/04_pig_latin/pig_latin.rb
@@ -1,1 +1,33 @@
 #write your code here
+def translate(s)
+  result = ""
+  sarray = s.split(" ")
+  for i in 0...sarray.length-1
+    result += translate_word(sarray[i]) + " "
+  end
+  result += translate_word(sarray[sarray.length-1])
+  return result
+end
+def translate_word(s)
+  sa = s.split(/(sch)/)
+  if sa.length > 1
+    return sa[2] + "schay"
+  end
+  sa = s.split(/([^aeiou]+)qu/)
+  if sa.length > 2
+    return sa[2] + sa[1] + "quay"
+  end
+  sa = s.split(/(qu)/)
+  if sa.length > 2
+    return sa[2] + "quay"
+  end
+  sa = s.split(/([aeiou].*)/)
+  if sa.length > 1
+    return sa[1] + sa[0] + "ay"
+  end
+  return s
+##  for i in 0...s.length
+##    if to_break_on.include?(s[i])
+##      first_vowel = i
+##    end
+  end
