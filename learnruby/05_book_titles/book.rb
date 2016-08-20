5808458b7f42099b9e8de0153bb7961029aac07f
Nathan McKinley-Pace <nmckinl3@gmu.edu>
2016-08-20 17:34:24 -0400

    Solved book_titles and timer

--- learnruby/05_book_titles/book.rb
+++ learnruby/05_book_titles/book.rb
@@ -1,3 +1,25 @@
 class Book
-# write your code here
+  # write your code here
+  @title = ""
+  def title
+    return @title
+  end
+  def title=(title)
+    t = ""
+    small_case_words = ["and", "a", "an", "or", "the", "up", "around", "over", "between", "through", "to", "about", "but", "for", "nor", "in", "the", "of"]
+    titlearray = title.split(" ")
+    for i in 0...titlearray.length
+      if i == 0
+        t += titlearray[i].capitalize
+      elsif small_case_words.include?(titlearray[i])
+        t += titlearray[i]
+      else
+        t += titlearray[i].capitalize
+      end
+      if i != titlearray.length-1
+        t += " "
+      end
+    end
+    @title = t
+  end
 end
--- learnruby/06_timer/timer.rb
+++ learnruby/06_timer/timer.rb
@@ -1,3 +1,49 @@
 class Timer
-  #write your code here
+  @time = 0
+  @time_string = ""
+  def initialize(t=0)
+    @time = t
+    update_time_string()
+  end
+  def seconds
+    return @time
+  end
+  def seconds=(value)
+    @time = value
+    update_time_string()
+  end
+  def minutes
+    return @time / 60
+  end
+  def minutes=(value)
+    @time = 60*value
+    update_time_string()
+  end
+  def hours
+    return @time/3600
+  end
+  def hours=(value)
+    @time = value * 3600
+    update_time_string()
+  end
+  def time_string
+    return @time_string
+  end
+  def update_time_string()
+    hours = pad(@time / 3600)
+    @time = @time - 3600*(hours.to_i)
+    minutes = pad(@time / 60)
+    @time = @time - 60*(minutes.to_i)
+    seconds = pad(@time)
+    @time = hours.to_i * 3600 + minutes.to_i * 60 + seconds.to_i
+    @time_string = "#{hours}:#{minutes}:#{seconds}"
+    return @time_string
+  end
+  def pad(value)
+    if value.to_s.length == 1
+      return "0" + value.to_s
+    else
+      return value.to_s
+    end
+  end
 end
